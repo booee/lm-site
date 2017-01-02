78951c9c3ce1227d9cde77b4b726fa7ccf708cf6
lennoxMac <lennoxmccaskill@gmail.com>
2017-01-01 20:57:37 -0500

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,45 +1,25 @@
 <?php
+$from = "lennoxmccaskill@gmail.com";
+$to = "lennoxmccaskill@gmail.com";
+$name = Trim(stripslashes($_POST['name']));
+$email = Trim(stripslashes($_POST['email']));
 
-if($_POST["submit"]) {
-    $recipient="lennoxmccaskill@gmail.com";
-    $subject="Someone Wants to Look Ahead";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    
+$body ="";
+$body .="Name: ";
+$body .=$name;
+$body .="\n";
+$body ="";
+$body .="Email: ";
+$body .=$email;
+$body .="\n";
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+$go = mail($to, $subject, $body, "From:<$from>");
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
+if($go) {
+	print("Success!");
+}
+else{
+	print("Unable to send!");
 }
 ?>
-<!DOCTYPE html>
-
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Contact form to email</title>
-</head>
-
-<body>
-
-    <?=$thankYou ?>
-
-    <form method="post" action="contact.php">
-        <label>Name:</label>
-        <input name="sender">
-
-        <label>Email address:</label>
-        <input name="senderEmail">
-
-        <label>Message:</label>
-        <textarea rows="5" cols="20" name="message"></textarea>
-
-        <input type="submit" name="submit">
-    </form>
-
-</body>
-
-</html>
-
